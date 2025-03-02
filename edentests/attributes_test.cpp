88898cf675c92205c9d8097920fab536821a32ad
soulseekerr <fremontv@gmail.com>
2025-03-02 18:03:08 +0000

    Attributes test

---
+++ edentests/attributes_test.cpp
@@ -0,0 +1,64 @@
+
+#include <gtest/gtest.h>
+
+#include "attributes.h"
+
+TEST(AttributesTest, DefaultConstructorDeleted) {
+    EXPECT_TRUE(std::is_default_constructible<eden::Attributes>::value == false);
+}
+
+TEST(AttributesTest, ConstructorAcceptingCobAsReference) {
+    eden::DateTime cob(2025, 1, 1);
+    eden::Attributes attributes(cob);
+    EXPECT_EQ(attributes.cob(), cob);
+}
+
+TEST(AttributesTest, CopyConstructor) {
+    eden::DateTime cob(2025, 1, 1);
+    eden::Attributes attributes(cob);
+    eden::Attributes copy(attributes);
+    EXPECT_EQ(copy.cob(), cob);
+}
+
+TEST(AttributesTest, MoveConstructor) {
+    eden::DateTime cob(2025, 1, 1);
+    eden::Attributes attributes(cob);
+    eden::Attributes move(std::move(attributes));
+    EXPECT_EQ(move.cob(), cob);
+}
+
+TEST(AttributesTest, AssignmentOperator) {
+    eden::DateTime cob(2025, 1, 1);
+    eden::Attributes attributes(cob);
+    eden::Attributes copy = attributes;
+    EXPECT_EQ(copy.cob(), cob);
+}
+
+TEST(AttributesTest, MoveAssignmentOperator) {
+    eden::DateTime cob(2025, 1, 1);
+    eden::Attributes attributes(cob);
+    eden::Attributes move = std::move(attributes);
+    EXPECT_EQ(move.cob(), cob);
+}
+
+TEST(AttributesTest, EqualityOperator) {
+    eden::DateTime cob(2025, 1, 1);
+    eden::Attributes attributes(cob);
+    eden::Attributes copy(attributes);
+    EXPECT_TRUE(attributes == copy);
+}
+
+TEST(AttributesTest, InequalityOperator) {
+    eden::DateTime cob(2025, 1, 1);
+    eden::Attributes attributes(cob);
+    eden::DateTime cob2(2025, 1, 2);
+    eden::Attributes copy(cob2);
+    EXPECT_TRUE(attributes != copy);
+}
+
+TEST(AttributesTest, GetterForCob) {
+    eden::DateTime cob(2025, 1, 1);
+    eden::Attributes attributes(cob);
+    EXPECT_EQ(attributes.cob(), cob);
+}
+
